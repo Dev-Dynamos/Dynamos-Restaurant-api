2e2af8823b88c33f641830b599226c0b594c9a90
menezescosta <mc611641@gmail.com>
2023-05-15 22:02:27 +0100

    chore: adicionando o eslint e o prettier

---
+++ src/application/usecases/CreateAdminUseCase.ts
@@ -0,0 +1,12 @@
+import { Admin } from "../../domain/entities/admin"
+
+type CreateAdminRequest={
+    name:string,
+    email:string,
+    password:string
+}
+
+export class CreateAdminUseCase{
+    execute({name,email,password}:CreateAdminRequest){
+        const admin=Admin.create({name,email,password})    }
+}
