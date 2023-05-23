b698e6f8e0b95f9dcb3eead74cb57a3128f682b4
menezescosta <mc611641@gmail.com>
2023-05-23 18:00:13 +0100

    feat: adicionando o clerk repository

--- src/modules/clerk/repository/IClerkRepository.ts
+++ src/modules/clerk/repository/IClerkRepository.ts
@@ -1,1 +1,6 @@
-interface;
+import { CreateClerkDTO } from "../ClerkDTOs/CreateClerkDTO";
+import { Clerk } from "../model/Clerk";
+
+export interface IClerkRepository {
+  create({ name }: CreateClerkDTO): Promise<Clerk>;
+}
