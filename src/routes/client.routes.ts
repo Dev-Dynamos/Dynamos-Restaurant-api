8142df1c0dfae47484623b909ccbc4a33d74116a
menezescosta <mc611641@gmail.com>
2023-06-01 02:50:04 +0100

    feat: adicionar o get client

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createClientController } from "../modules/client/controllers/createClient";
+import { getClientController } from "../modules/client/controllers/getClient";
 
 const clientRoutes = Router();
 
 clientRoutes.post("/", (req, res) => {
   return createClientController.handle(req, res);
+});
+
+clientRoutes.get("/", (req, res) => {
+  return getClientController.handle(req, res);
 });
 
 export { clientRoutes };
