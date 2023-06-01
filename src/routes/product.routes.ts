461020296e3fad6944d059f331bf3d850337312f
menezescosta <mc611641@gmail.com>
2023-06-01 10:44:40 +0100

    feat: adicionando get product route

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createProductController } from "../modules/product/controllers/createProduct";
+import { getProductController } from "../modules/product/controllers/getProduct";
 
 const productRoutes = Router();
 
 productRoutes.post("/", (req, res) => {
   return createProductController.handle(req, res);
+});
+
+productRoutes.get("/", (req, res) => {
+  return getProductController.handle(req, res);
 });
 
 export { productRoutes };
