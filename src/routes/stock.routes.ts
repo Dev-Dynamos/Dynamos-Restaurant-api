0c605de8445debded1bb1291eecbfc92d3f181dc
menezescosta <mc611641@gmail.com>
2023-06-01 10:33:57 +0100

    feat: adicionando get stock route

--- src/routes/stock.routes.ts
+++ src/routes/stock.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createStockController } from "../modules/stock/controllers/createStock";
+import { getStockController } from "../modules/stock/controllers/getAdmin";
 
 const stockRoutes = Router();
 
 stockRoutes.post("/", (req, res) => {
   return createStockController.handle(req, res);
+});
+
+stockRoutes.get("/", (req, res) => {
+  return getStockController.handle(req, res);
 });
 
 export { stockRoutes };
