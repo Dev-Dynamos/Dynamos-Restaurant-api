3341daa7e032008be412dfc99b4c0d792fb7209c
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-06-29 20:25:39 +0100

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { errorHandler } from "./middlewares/errorHandler";
 import { router } from "./routes";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
+app.use(cors)
 
 export {app};
