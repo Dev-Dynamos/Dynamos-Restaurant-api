4d2a7843564a92de8d8a5842789c3462e932de8b
fabiojunik <fabiojunik@gmail.com>
2023-06-29 20:10:10 +0100

    chore: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import { errorHandler } from "./middlewares/errorHandler";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.use(errorHandler);
 
