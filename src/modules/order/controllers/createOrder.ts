ff4394250e1b34291299141d377b9fae6d55a8d8
fabiojunik <fabiojunik@gmail.com>
2023-07-21 13:10:00 +0100

    feat: Put produt and manu opitional fields

--- src/modules/order/controllers/createOrder.ts
+++ src/modules/order/controllers/createOrder.ts
@@ -1,9 +1,22 @@
+import { ClientRepository } from "../../client/repository/implement/ClientRepository";
+import { MenuRepository } from "../../menu/repository/implements/MenuRepository";
+import { ProductRepository } from "../../product/repository/implement/ProductRepository";
 import { OrderRepository } from "../repository/implements/OrderRepository";
 import { CreateOrderUseCase } from "../useCases/CreateOrderUseCase";
 import { CreateOrderController } from "./CreateOrderController";
 
 const orderRepository = new OrderRepository();
-const createOrderUseCase = new CreateOrderUseCase(orderRepository);
+const clientRepository = new ClientRepository();
+const productRepository = new ProductRepository();
+const menuRepository = new MenuRepository();
+
+const createOrderUseCase = new CreateOrderUseCase(
+    orderRepository, 
+    clientRepository, 
+    productRepository,
+    menuRepository
+);
+
 const createOrderController = new CreateOrderController(createOrderUseCase);
 
 export { createOrderController };
--- src/modules/order/useCases/CreateOrderUseCase.ts
+++ src/modules/order/useCases/CreateOrderUseCase.ts
@@ -1,8 +1,17 @@
+import { AppError } from "../../../errors/AppError";
+import { IClientRepository } from "../../client/repository/IClientRepository";
+import { IMenuRepository } from "../../menu/repository/IMenuRepository";
+import { IProductRepository } from "../../product/repository/IProductRepository";
 import { CreateOrderDTO } from "../OrderDTOs/CreateOrderDTO";
 import { IOrderRepository } from "../repository/IOrderRepository";
 
 export class CreateOrderUseCase {
-  constructor(private orderRepository: IOrderRepository) {}
+  constructor(
+    private orderRepository: IOrderRepository,
+    private clientRepository: IClientRepository,
+    private productRepository: IProductRepository,
+    private menuRepository: IMenuRepository
+  ) { }
 
   async execute({
     status,
@@ -12,6 +21,22 @@ export class CreateOrderUseCase {
     productoId,
     menuId,
   }: CreateOrderDTO) {
+
+    const clientExists = await this.clientRepository.findById(clienteId);
+
+    if (!clientExists)
+      throw new AppError("Cliente não encontrado !", 404);
+
+    const productExists = productoId && await this.productRepository.findById(productoId);
+
+    if (productoId && !productExists)
+      throw new AppError("Produto não encontrado !", 404);
+
+    const menuExists = menuId && await this.menuRepository.findById(menuId)
+
+    if (menuId && !menuExists)
+      throw new AppError("Menu não encontrado !", 404);
+
     const order = await this.orderRepository.create({
       descricao,
       localizacao,
