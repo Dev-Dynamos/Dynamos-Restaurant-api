eb8d138ba1093d5e90a17acb163a84a1a4d2b891
fabiojunik <fabiojunik@gmail.com>
2023-08-02 15:50:52 +0100

    feat(order): update order router

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getOrderController } from "../modules/order/controllers/getOrders";
 import { createOrderController } from "../modules/order/controllers/createOrder";
 import { deleteOrderController } from "../modules/order/controllers/deleteOrder";
+import { updateOrderController } from "../modules/order/controllers/updateOrder";
 
 const orderRoutes = Router();
 
@@ -15,6 +16,10 @@ orderRoutes.get("/", (req, res) => {
 
 orderRoutes.delete("/:id", (req, res) => {
   return deleteOrderController.handle(req, res);
+});
+
+orderRoutes.put("/:id", (req, res) => {
+  return updateOrderController.handle(req, res);
 });
 
 export { orderRoutes };
